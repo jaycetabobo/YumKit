e77877b164b3fa88d4f6398521944bef7d237370
Marlon Tumamak <barina101xd@gmail.com>
2023-10-21 09:06:40 +0800

    added some code in data.js

--- Data/data.js
+++ Data/data.js
@@ -1,15 +1,26 @@
 import { useState } from "react";
+export default function Data() {
+  const [data, setData] = useState([]);
 
-export default function Data(){
-    const [data, setData] = useState([{
-        'fullname': '',
-        'birthdate': '',
-        'location': '',
-        'phonenumber': '',
-        'username': '',
-        'email': '',
-        'password': '',
-        'confirmpassword': '',
-}]);
+  return data;
+}
+export const users = [
+  { username: "user1", password: "password1" },
+  { username: "user2", password: "password2" },
+  // Add more users as needed
+];
 
-};
+// export default function Data() {
+//     const [data, setData] = useState([
+//       {
+//         fullname: "",
+//         birthdate: "",
+//         location: "",
+//         phonenumber: "",
+//         username: "",
+//         email: "",
+//         password: "",
+//         confirmpassword: "",
+//       },
+//     ]);
+//   }
