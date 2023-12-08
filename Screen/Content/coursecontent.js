f262498d4309d840be9a63fb59f11fe538fbeb71
Jonie Boy Sumalpong <sumalpongjonieboy@gmail.com>
2023-12-08 13:59:49 +0800

    Add text Favorites

--- Screen/Content/coursecontent.js
+++ Screen/Content/coursecontent.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import { ImageBackground, Text, View } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 
 export default function Coursecontent() {
   return (
     <View>
-        <View style={{width: 375, height: 500, alignItems: 'center'}}> 
+        <View style={{width: 375, alignItems: 'center'}}> 
         <ImageBackground source={require("../../assets/bannerimage2.png")}
-        style={{width:300, height: 150, alignItems:'center', justifyContent: 'center'}}> 
-        <Text>Java</Text>
+        style={{width:270, height: 120, alignItems:'center', justifyContent: 'center'}}> 
+        <Text style={{fontFamily:"glacialindibold", fontSize: 50}}>Java</Text>
         </ImageBackground>
+        </View>
+        <View style={{width:375, alignItems:'center', justifyContent: 'center'}}>
+            <TouchableOpacity style={{width:105, backgroundColor: "#D9D9D9", alignItems: 'center', padding: 10}}>
+                <Text>Add Favorites</Text>
+            </TouchableOpacity>
         </View>
     </View>
   )
