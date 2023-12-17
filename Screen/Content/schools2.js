e3832e862fac25c5bb907f3b21ffc6b3938c920b
Marlon Tumamak <marlontumamak1@gmail.com>
2023-12-17 19:18:19 +0800

    Added contents for schools2.js

--- Screen/Content/schools2.js
+++ Screen/Content/schools2.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { Dimensions, Image, ImageBackground, Text, View } from "react-native";
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  Text,
+  Touchable,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 const { width, height } = Dimensions.get("window");
 function Schools2() {
@@ -26,6 +35,54 @@ function Schools2() {
       >
         <Text>BS in Information Technology</Text>
       </ImageBackground>
+      <ScrollView
+        style={{
+          backgroundColor: "pink",
+          width: "98%",
+          height: 350,
+          marginTop: 20,
+        }}
+      >
+        <Text>Programming</Text>
+        <View
+          style={{
+            height: 170,
+            width: 270,
+            backgroundColor: "white",
+            borderRadius: 20,
+            marginTop: 10,
+          }}
+        >
+          <Text>This is for image</Text>
+        </View>
+        <Text style={{ marginTop: 10 }}>
+          Java is a high-level, class-based, object-oriented programming
+          language. Developers use Java to construct applications in laptops,
+          data centres, game consoles, scientific supercomputers, cell phones,
+          and other devices.
+        </Text>
+        <TouchableOpacity>
+          <View
+            style={{
+              backgroundColor: "black",
+              width: 120,
+              borderRadius: 20,
+              padding: 5,
+              marginTop: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 17,
+                textAlign: "center",
+                color: "white",
+              }}
+            >
+              View Now
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 }
