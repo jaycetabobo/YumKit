7569ef9275b75a037ec3a36ef436b784702e5ff6
Marlon Tumamak <marlontumamak1@gmail.com>
2023-12-17 18:51:13 +0800

    Edit and add contents in schools2.js

--- Screen/Content/schools2.js
+++ Screen/Content/schools2.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Dimensions, Image, ImageBackground, Text, View } from "react-native";
 
+const { width, height } = Dimensions.get("window");
 function Schools2() {
   return (
-    <View>
-      <Text> Welcome, my friend!</Text>
+    <View style={{ alignItems: "center" }}>
+      <View style={{ width: "98%" }}>
+        <Text style={{ textAlign: "center" }}>
+          University of Science and Technology of Southern Philippines
+        </Text>
+      </View>
+      <Image
+        source={require("../../assets/ustp2.jpg")}
+        style={{ width: "98%", height: 150, marginTop: 30, borderRadius: 30 }}
+      />
     </View>
   );
 }
