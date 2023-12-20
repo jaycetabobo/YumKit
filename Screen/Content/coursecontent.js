a3b48b1cc56030f4a57d37e77f01b7f09ce21ee4
Jonie Boy Sumalpong <sumalpongjonieboy@gmail.com>
2023-12-20 14:50:38 +0800

    initial output

--- Screen/Content/coursecontent.js
+++ Screen/Content/coursecontent.js
@@ -50,7 +50,7 @@ It is used for:</Text>
           - And much, much more!
         </Text>
         <Text style={{fontSize: 20, marginTop: 15, padding: 5, fontFamily: "glacialindi"}}>
-Why Use Java?
+Why we should use Java?
 Java works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc.)
 It is one of the most popular programming language in the world
 It has a large demand in the current job market
@@ -59,7 +59,7 @@ It is open-source and free
 It is secure, fast and powerful
 It has a huge community support (tens of millions of developers)
 Java is an object oriented language which gives a clear structure to programs and allows code to be reused, lowering development costs
-As Java is close to C++ and C#, it makes it easy for programmers to switch to Java or vice versa
+As Java is close to C++ and C#, it makes it easy for programmers to switch to Java or vice versa.
 
             </Text>
             
