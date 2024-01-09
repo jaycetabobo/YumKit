9a5c739d9b1ddf204e5daf0c3e03252d978aad33
Kenneth Angel Sayan <sayankennethangel@gmail.com>
2024-01-09 16:23:27 +0800

    Style in view

--- Screen/Content/notification.js
+++ Screen/Content/notification.js
@@ -5,6 +5,32 @@ const { width, height } = Dimensions.get("window");
 export default function Notification() {
   return (
     <SafeAreaView>
+      <View
+        style={{
+          
+          height: height,
+          width: width,
+          backgroundColor: "white"
+        }}
+      >
+        <View
+          style={{
+            fontFamily: "glacialindi",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginRight: 10,
+            marginLeft: 10,
+            alignItems: "center",
+            marginBottom:10,
+            borderRadius:10,
+            borderBottomWidth: 2,
+            borderBottomColor: 'gray',
+            padding:5
+
+          }}
+        >
+         </View>
+      </View>
     </SafeAreaView>
   );
 }
