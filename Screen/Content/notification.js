4aa4fef57ba840d828c90722c4a5e0908640ade2
Kenneth Angel Sayan <sayankennethangel@gmail.com>
2024-01-09 16:30:34 +0800

    Logo background and Notification Text

--- Screen/Content/notification.js
+++ Screen/Content/notification.js
@@ -44,8 +44,39 @@ export default function Notification() {
               height: 18,
               marginTop:10
             }}
+          />
+          <ImageBackground
+            source={require("../../assets/logo-no-background.png")}
+            style={{
+              width: 30,
+              height: 37,
+              
+            }}
             
           />
+          </View>
+          <Text
+            style={{
+              fontSize: 25,
+              fontFamily: "carme",
+              lineheight: "normal",
+              fontWeight:'400',
+              alignItems: "center"
+
+            }}
+          >
+            Notification
+          </Text>
+          
+          <View style={{
+                borderRadius:200/2,
+                height: 32,
+                width: 32,
+                borderWidth:1,
+
+
+          }}
+          >
           </View>
          </View>
       </View>
